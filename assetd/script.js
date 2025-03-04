a85b49457649ccad35ce55b255f083e13e6615fa
cihankarimli <kerimlicihan1@gmail.com>
2025-03-04 18:26:53 +0400

    deyisdirildi

---
+++ assetd/script.js
@@ -0,0 +1,14 @@
+//let name="john"
+//let admin=name
+//console.log(admin)
+//alert("Merhaba ben javascript")
+// Hata yok
+/*let mesaj = "merhaba";
+mesaj = 123456;*/
+// let number="$" + 4 + 5
+//  console.log(number);
+// let/ a = 1, b = 1;
+
+// let c = ++a; // ?
+// let d = b++; // ?
+// console.log(a,b,c,d)
