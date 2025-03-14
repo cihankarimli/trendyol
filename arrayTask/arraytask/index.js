7dfe3d5c038761fbc0fed0e4ff698f44d7911a58
cihankarimli <kerimlicihan1@gmail.com>
2025-03-14 17:56:37 +0400

    deyisdirildi

--- arrayTask/arraytask/index.js
+++ arrayTask/arraytask/index.js
@@ -2,7 +2,7 @@ let students = [
     { name: 'Alice', age: 20, score: 85 },
     { name: 'Bob', age: 22, score: 92 },
     { name: 'Charlie', age: 21, score: 78 },
-    { name: 'Dave', age: 24, score: 90 },
+    { name: 'Dave', age: 24, score: 95 },
     { name: 'Eve', age: 23, score: 88 },
     { name: 'Frank', age: 20,score:80}
   ];
@@ -23,18 +23,94 @@ let students = [
    
    
 // }
-// console.log(maxscore);
-let array=[]
-let maxscore=students[0].score
-for (let f = 0; f < students.length; f++) {
-    
-    for (let i = 0; i < students.length; i++) {
-        if (maxscore<students[f].score) {
-            array.push(students[f].score)
-        }
-        
-    }
-    console.log(array);
-    
-}
 
+
+// console.log(maxscore);
+
+
+// let array=[]
+// let maxscore=students[0].score
+// for (let f = 0; f < students.length; f++) {
+    
+//     for (let i = 0; i < students.length; i++) {
+//         if (maxscore<students[f].score) {
+//             maxscore=students[f].score
+//             array.push(maxscore)
+//         }
+        
+//     }
+    
+// }
+// console.log(array);
+
+
+
+// function passedStudent() {
+//     let Array=[]
+//     for (let i = 0; i < students.length; i++) {
+//        if (students[i].score>=85){
+//         Array.push(students[i].name)
+        
+//     }
+    
+// }
+// console.log(Array);
+// }
+// passedStudent()
+
+
+
+
+
+// let student=students[0]
+// function oldestStudent() {
+//   let studentAge=students[0].age
+//   let array=[]
+//    for (let i = 1; i<students.length; i++) {
+//    if(studentAge<students[i].age){
+//     studentAge=students[i].age
+//   student=students[i]
+// }
+
+
+
+
+// }
+// console.log(student);
+
+
+// }
+// oldestStudent();
+
+
+
+
+// function highScore(){
+//   for (let i = 0; i < students.length; i++) {
+//   if(students[i].score>=95)
+//          console.log(students[i],'=>',true);
+//      else{
+//       console.log(students[i],'=>',false);
+      
+//      }    
+//   } 
+
+  
+    
+  
+ 
+ 
+// }
+// highScore();
+
+
+
+
+// function listStudentsname() {
+//   for (let i = 0; i < students.length; i++) {
+//    console.log(students[i].name);
+   
+    
+//   }
+// }
+// listStudentsname();
